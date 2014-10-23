bff9eee7bc244f00917ce648e87ab6a96d0db2c6
Ubuntu <ubuntu@ip-172-31-11-2.us-west-1.compute.internal>
2014-10-23 01:07:52 +0000

    Frantic plane commit

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/davecgh/go-spew/spew"
-	apiClient "github.com/fsouza/go-dockerclient"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 )
@@ -27,18 +26,20 @@ type GithubPushEventPayload struct {
 }
 
 func main() {
-	dockerHost := os.Getenv("DOCKER_HOST")
-	if dockerHost == "" {
-		dockerHost = "unix:///var/run/docker.sock"
+	homeDir := os.Getenv("HOME")
+	if _, err := os.Stat(fmt.Sprintf("%s/.dockercfg", homeDir)); err != nil {
+		if os.IsNotExist(err) {
+			cmd := exec.Command("docker", "login")
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := exec.Command("docker", "login").Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error running docker login")
+				os.Exit(1)
+			}
+		}
 	}
-
-	_, err := apiClient.NewClient(dockerHost)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating client!!", err)
-	}
-
 	r := render.New(render.Options{})
-
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		var (
@@ -50,7 +51,8 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error decoding Github push payload:", err)
 		}
 		spew.Dump(payload)
-		repoPath := fmt.Sprintf("./repos/%s", payload.Repository.FullName)
+		fullName := payload.Repository.FullName
+		repoPath := fmt.Sprintf("./repos/%s", fullName)
 		repoUrl := payload.Repository.HtmlUrl
 		if _, err := os.Stat(repoPath); err != nil {
 			if os.IsNotExist(err) {
@@ -63,8 +65,21 @@ func main() {
 			}
 		} else {
 			os.Chdir(repoPath)
+			fmt.Println("Pulling existing repository")
 			if err := exec.Command("git", "pull").Run(); err != nil {
 				fmt.Fprintln(os.Stderr, "Error pulling git repository:", err)
+				r.JSON(w, http.StatusInternalServerError, "")
+			}
+
+			fmt.Println("Building docker image")
+			// TODO: make it so that user name can be different than on Github
+			if err := exec.Command("docker", "build", "-t", fullName, ".").Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error building docker image for", fullName, ":", err)
+			}
+
+			fmt.Println("Pushing image back to Docker Hub")
+			if err := exec.Command("docker", "push", fullName).Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error pushing docker image for", fullName, ":", err)
 			}
 		}
 
@@ -73,5 +88,5 @@ func main() {
 
 	n := negroni.Classic()
 	n.UseHandler(router)
-	n.Run(":3000")
+	n.Run(":80")
 }
