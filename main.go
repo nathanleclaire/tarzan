07c44443d7304801928014f5ba7d510fc297f5c7
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-10-26 23:38:55 -0400

    Use codegangsta's cli package

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/codegangsta/cli"
 	"github.com/codegangsta/negroni"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
@@ -59,74 +60,84 @@ func main() {
 	logging.SetFormatter(logging.MustStringFormatter(format))
 	logging.SetLevel(logging.NOTICE, "streamLog")
 	homeDir := os.Getenv("HOME")
-	if _, err := os.Stat(fmt.Sprintf("%s/.dockercfg", homeDir)); err != nil {
-		if os.IsNotExist(err) {
-			log.Warning("Detected no Docker Hub login.  Please log in now.")
-			cmd := exec.Command("docker", "login")
-			cmd.Stdin = os.Stdin
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Run(); err != nil {
-				fmt.Fprintln(os.Stderr, "Error running docker login")
-				os.Exit(1)
+
+	app := cli.NewApp()
+	app.Name = "tarzan"
+	app.Usage = "naive cached automated build implementation"
+
+	app.Action = func(c *cli.Context) {
+		if _, err := os.Stat(fmt.Sprintf("%s/.dockercfg", homeDir)); err != nil {
+			if os.IsNotExist(err) {
+				log.Warning("Detected no Docker Hub login.  Please log in now.")
+				cmd := exec.Command("docker", "login")
+				cmd.Stdin = os.Stdin
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				if err := cmd.Run(); err != nil {
+					fmt.Fprintln(os.Stderr, "Error running docker login")
+					os.Exit(1)
+				}
 			}
 		}
-	}
-	r := render.New(render.Options{})
-	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		var (
-			payload GithubPushEventPayload
-		)
-		decoder := json.NewDecoder(req.Body)
-		err := decoder.Decode(&payload)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error decoding Github push payload:", err)
-		}
-		spew.Dump(payload)
-		fullName := payload.Repository.FullName
-		repoPath := fmt.Sprintf("./repos/%s", fullName)
-		repoUrl := payload.Repository.HtmlUrl
-		if _, err := os.Stat(repoPath); err != nil {
-			if os.IsNotExist(err) {
-				fmt.Println("Executing command", "git clone --recursive", repoUrl, repoPath)
-				if err := exec.Command("git", "clone", "--recursive", repoUrl, repoPath).Run(); err != nil {
-					fmt.Fprintln(os.Stderr, "Error cloning git repository:", err)
+		r := render.New(render.Options{})
+		router := mux.NewRouter()
+		router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+			var (
+				payload GithubPushEventPayload
+			)
+			decoder := json.NewDecoder(req.Body)
+			err := decoder.Decode(&payload)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error decoding Github push payload:", err)
+			}
+			spew.Dump(payload)
+			fullName := payload.Repository.FullName
+			repoPath := fmt.Sprintf("./repos/%s", fullName)
+			repoUrl := payload.Repository.HtmlUrl
+			if _, err := os.Stat(repoPath); err != nil {
+				if os.IsNotExist(err) {
+					fmt.Println("Executing command", "git clone --recursive", repoUrl, repoPath)
+					if err := exec.Command("git", "clone", "--recursive", repoUrl, repoPath).Run(); err != nil {
+						fmt.Fprintln(os.Stderr, "Error cloning git repository:", err)
+					}
+				} else {
+					fmt.Fprintln(os.Stderr, "Error stat-ing directory", repoPath, ":", err)
 				}
 			} else {
-				fmt.Fprintln(os.Stderr, "Error stat-ing directory", repoPath, ":", err)
-			}
-		} else {
-			os.Chdir(repoPath)
-			fmt.Println("Pulling existing repository")
-			if err := exec.Command("git", "pull").Run(); err != nil {
-				fmt.Fprintln(os.Stderr, "Error pulling git repository:", err)
-				r.JSON(w, http.StatusInternalServerError, "")
+				os.Chdir(repoPath)
+				fmt.Println("Pulling existing repository")
+				if err := exec.Command("git", "pull").Run(); err != nil {
+					fmt.Fprintln(os.Stderr, "Error pulling git repository:", err)
+					r.JSON(w, http.StatusInternalServerError, "")
+				}
+
+				fmt.Println("Building docker image")
+				err := streamCommand("docker", "build", "-t", fullName, ".")
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Error building docker image for", fullName, ":", err)
+					r.JSON(w, http.StatusInternalServerError, map[string]interface{}{
+						"Error": err,
+					})
+				}
+
+				fmt.Println("Pushing image back to Docker Hub")
+				err = streamCommand("docker", "push", fullName)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Error pushing docker image for", fullName, ":", err)
+					r.JSON(w, http.StatusInternalServerError, map[string]interface{}{
+						"Error": err,
+					})
+				}
 			}
 
-			fmt.Println("Building docker image")
-			err := streamCommand("docker", "build", "-t", fullName, ".")
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error building docker image for", fullName, ":", err)
-				r.JSON(w, http.StatusInternalServerError, map[string]interface{}{
-					"Error": err,
-				})
-			}
+			r.JSON(w, http.StatusOK, "")
+		}).Methods("POST")
 
-			fmt.Println("Pushing image back to Docker Hub")
-			err = streamCommand("docker", "push", fullName)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error pushing docker image for", fullName, ":", err)
-				r.JSON(w, http.StatusInternalServerError, map[string]interface{}{
-					"Error": err,
-				})
-			}
-		}
+		n := negroni.Classic()
+		n.UseHandler(router)
+		n.Run(":80")
 
-		r.JSON(w, http.StatusOK, "")
-	}).Methods("POST")
+	}
 
-	n := negroni.Classic()
-	n.UseHandler(router)
-	n.Run(":80")
+	app.Run(os.Args)
 }
